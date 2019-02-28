a25fa0a8a6c9b6b4d6706b046222d79ad1388cac
Alex Crichton <alex@alexcrichton.com>
2019-02-28 07:43:02 -0800

    Initial commit

---
+++ build.sh
@@ -0,0 +1,8 @@
+set -ex
+
+cargo build --release --target wasm32-unknown-unknown
+cp target/wasm32-unknown-unknown/release/rust_fannkuch_js_and_wasm.wasm rust.wasm
+wasm-strip rust.wasm
+wasm-opt -O3 rust.wasm -o out.wasm
+cp out.wasm rust.wasm
+python -m SimpleHTTPServer
